c65a110f0cf84f26825d9a332a70962fafb98cb1
edukleber100 <edukleber100@gmail.com>
2023-10-19 18:54:20 -0300

    listando despesas

--- js/app.js
+++ js/app.js
@@ -40,6 +40,29 @@ class Bd {
 
         localStorage.setItem('id', id)
     }
+    
+    recuperarTodosRegistros(){
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperar as despesas cadastradas
+        for(let i = 1; i <= id; i++){
+
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //pular índices removidos
+            if(despesa === null){
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+
+        return despesas
+    }
 }
 
 let bd = new Bd()
@@ -66,17 +89,75 @@ function cadastrarDespesa(){
    
     if(despesa.validarDados()){
         bd.gravar(despesa)
-        $('#mensagem').text("Registro inserido com sucesso").css('color', 'green');
-        $('#mensagemec').text("Despesa foi cadastrada com sucesso").css('color', 'black');
+        
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
         $('#registraDespesa').modal('show')
     } else {
-        $('#mensagem').text("Registro inválido").css('color', 'red');
-        $('#mensagemec').text("Erro ao inserir registro, verifique se os campos foram preenchidos corretamente!");
-        $('#botao').text('Voltar e corrigir').removeClass('btn-success').addClass('btn-danger')
+        
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
         $('#registraDespesa').modal('show')
     }
     
 
+}
+
+function carregaListaDespesas(){
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    let listasDespesas = document.getElementById('listasDespesas')
+
+    /*
+                <tr>
+                    <td>15/03/2018</td>
+                    <td>Alimentação</td>
+                    <td>Compras do mês</td>
+                    <td>444.75</td>
+                </tr>
+              
+    */ 
+
+   despesas.forEach(function(d){
+    
+    //criando a linha (tr)
+    let linha = listasDespesas.insertRow()
+
+    //colunas
+    linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+    //ajustar o tipo
+    switch(d.tipo){
+        case '1': d.tipo = 'Alimentação'
+            break
+        case '2': d.tipo = 'Educação'
+            break
+        case '3': d.tipo = 'Lazer'
+            break
+        case '4': d.tipo = 'Saúde'
+            break
+        case '5': d.tipo = 'Transporte'
+            break
+        case '6': d.tipo = 'Outros'
+            break
+    }
+
+    linha.insertCell(1).innerHTML = d.tipo
+    linha.insertCell(2).innerHTML = d.descricao
+    linha.insertCell(3).innerHTML = d.valor
+
+   })
 }
 
 
