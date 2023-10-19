6e1d4bc3697ef1328fd47712600ffaf4d729f178
edukleber100 <edukleber100@gmail.com>
2023-10-19 12:17:05 -0300

    cadastrando despesas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+class Despesa{
+    constructor(ano, mes, dia, tipo, descricao, valor){
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
 function cadastrarDespesa(){
     
    let ano = document.getElementById('ano')
@@ -8,4 +19,21 @@ function cadastrarDespesa(){
    let valor = document.getElementById('valor')
 
    console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+   let despesa = new Despesa(
+    ano.value, 
+    mes.value, 
+    dia.value, 
+    tipo.value, 
+    descricao.value, 
+    valor.value
+   )
+   
+    gravar(despesa)
+
 }
+
+function gravar(d) {
+    localStorage.setItem('despesa', JSON.stringify(d))
+}
+
